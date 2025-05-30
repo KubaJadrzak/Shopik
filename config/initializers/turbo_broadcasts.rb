f9e9d6c4896a7705583306d4f273a4d3bdd0c98c
kuba <kuba.jadrzak@gmail.com>
2025-05-30 22:33:10 +0200

    fix the issue with turbo broadcast feature not working with sidekiq by adding an initializer file

---
+++ config/initializers/turbo_broadcasts.rb
@@ -0,0 +1,6 @@
+# this fix was implemented since sidekiq didn't work with turbo 8 broadcasts_refreshes properly
+Rails.application.config.after_initialize do
+  Turbo::Streams::BroadcastStreamJob.class_eval do
+    self.queue_adapter = :async
+  end
+end
