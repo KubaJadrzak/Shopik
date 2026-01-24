744e80db8f71767ea4411e152dbce01e26826527
KubaJadrzak <kuba.jadrzak@gmail.com>
2026-01-24 15:48:19 +0100

    fix rubocop

--- db/migrate/20260124133252_add_espago_payment_token_to_payments.rb
+++ db/migrate/20260124133252_add_espago_payment_token_to_payments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddEspagoPaymentTokenToPayments < ActiveRecord::Migration[8.0]
   def change
     add_column :payments, :espago_payment_token, :string, null: true
